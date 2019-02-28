092d48ccaad051686177004382fe6b148072c8c4
Alexandre Tessier <alexandre.tessier@hotmail.fr>
2019-02-28 17:21:00 -0500

    Installations des composantes de base et d'Acceuil et Probleme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { AccueilComponent } from './accueil/accueil.component';
+import { ProblemeComponent } from './probleme/probleme.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent,
+    ProblemeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFontAwesomeModule,
+    RouterModule.forRoot([
+      {path: 'accueil',component:AccueilComponent},
+      {path: 'probleme',component:ProblemeComponent},
+      {path: '', redirectTo:'accueil',pathMatch:'full'},
+      {path: '**', redirectTo:'accueil',pathMatch:'full'}
+    ])    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/probleme/probleme.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'Inter-probleme',
+  templateUrl: './probleme.component.html',
+  styleUrls: ['./probleme.component.css']
+})
+export class ProblemeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
